b8acd8d3575c618239ef469378665ab4abf7ab05
Pawel <pawelgt99@gmail.com>
2020-05-27 23:40:40 +0200

    Delete //TODO

---
+++ Cw12/Models/s18923Context.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Cw12.Models
+{
+    public partial class s18923Context : DbContext
+    {
+        public s18923Context()
+        {
+        }
+
+        public s18923Context(DbContextOptions<s18923Context> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Doctor> Doctor { get; set; }
+        public virtual DbSet<Medicament> Medicament { get; set; }
+        public virtual DbSet<Patient> Patient { get; set; }
+        public virtual DbSet<Prescription> Prescription { get; set; }
+        public virtual DbSet<PrescriptionMedicament> PrescriptionMedicament { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Data Source=db-mssql;Initial Catalog=s18923;Integrated Security=True");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Doctor>(entity =>
+            {
+                entity.HasKey(e => e.IdDoctor)
+                    .HasName("Doctor_pk");
+
+                entity.Property(e => e.Email)
+                    .IsRequired()
+                    .HasMaxLength(100);
+
+                entity.Property(e => e.FirstName)
+                    .IsRequired()
+                    .HasMaxLength(100);
+
+                entity.Property(e => e.LastName)
+                    .IsRequired()
+                    .HasMaxLength(100);
+            });
+
+            modelBuilder.Entity<Medicament>(entity =>
+            {
+                entity.HasKey(e => e.IdMedicament)
+                    .HasName("Medicament_pk");
+
+                entity.Property(e => e.Description)
+                    .IsRequired()
+                    .HasMaxLength(100);
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(100);
+
+                entity.Property(e => e.Type)
+                    .IsRequired()
+                    .HasMaxLength(100);
+            });
+
+            modelBuilder.Entity<Patient>(entity =>
+            {
+                entity.HasKey(e => e.IdPatient)
+                    .HasName("Patient_pk");
+
+                entity.Property(e => e.Birthdate).HasColumnType("date");
+
+                entity.Property(e => e.FirstName)
+                    .IsRequired()
+                    .HasMaxLength(100);
+
+                entity.Property(e => e.LastName)
+                    .IsRequired()
+                    .HasMaxLength(100);
+            });
+
+            modelBuilder.Entity<Prescription>(entity =>
+            {
+                entity.HasKey(e => e.IdPrescription)
+                    .HasName("Prescription_pk");
+
+                entity.Property(e => e.Date).HasColumnType("date");
+
+                entity.Property(e => e.DueDate).HasColumnType("date");
+
+                entity.HasOne(d => d.IdDoctorNavigation)
+                    .WithMany(p => p.Prescription)
+                    .HasForeignKey(d => d.IdDoctor)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("Prescription_Doctor");
+
+                entity.HasOne(d => d.IdPatientNavigation)
+                    .WithMany(p => p.Prescription)
+                    .HasForeignKey(d => d.IdPatient)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("Prescription_Patient");
+            });
+
+            modelBuilder.Entity<PrescriptionMedicament>(entity =>
+            {
+                entity.HasKey(e => new { e.IdMedicament, e.IdPrescription })
+                    .HasName("Prescription_Medicament_pk");
+
+                entity.ToTable("Prescription_Medicament");
+
+                entity.Property(e => e.Details)
+                    .IsRequired()
+                    .HasMaxLength(100);
+
+                entity.HasOne(d => d.IdMedicamentNavigation)
+                    .WithMany(p => p.PrescriptionMedicament)
+                    .HasForeignKey(d => d.IdMedicament)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("Prescription_Medicament_Medicament");
+
+                entity.HasOne(d => d.IdPrescriptionNavigation)
+                    .WithMany(p => p.PrescriptionMedicament)
+                    .HasForeignKey(d => d.IdPrescription)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("Prescription_Medicament_Prescription");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ Cw12/Services/DbService.cs
@@ -0,0 +1,57 @@
+﻿using Cw12.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cw12.Services
+{
+    public class DbService : IDbService
+    {
+        private s18923Context context;
+
+        public DbService(s18923Context context)
+        {
+            this.context = context;
+        }
+
+        public void DeletePatient(Patient patient)
+        {
+            var result = context.Patient.FirstOrDefault(e => e.IdPatient == patient.IdPatient);
+            //var result = context.Patient.Include(x => x.Prescription).FirstOrDefault(e => e.IdPatient == patient.IdPatient);
+            //var pre = context.Prescription.Where(s => s.IdPatient == result.IdPatient).ToList();      
+            context.Patient.Remove(result);
+            ////context.Prescription.Remove(pre);
+            context.SaveChanges();
+        }
+
+        public void AddPatient(Patient patient)
+        {
+            context.Add(patient);
+            context.SaveChanges();
+        }
+
+        public Patient GetPatientDetails(Patient patient)
+        {
+            //var xxxx = context
+            //    .Patient
+            //    .Where(e => e.IdPatient == patient.IdPatient)
+            //    .Select(x => new 
+            //    {
+            //        x.LastName,
+            //        x.IdPatient,
+            //        Recepty = x.Prescription.Select(s => new { s.IdPrescription, s.IdDoctorNavigation.LastName, s.IdDoctorNavigation.FirstName })
+            //    })
+            //    .ToList();
+
+            var result = context.Patient.Include(x => x.Prescription).FirstOrDefault(e => e.IdPatient == patient.IdPatient);                                 
+            return result;
+        }
+
+        public List<Patient> GetPatients()
+        {
+            return context.Patient.ToList();
+        }
+    }
+}
